0ce26472d5b63e213c6867ea1194c45079b00112
neldeles <neldeles@gmail.com>
2022-02-14 19:09:22 +0800

    feat: create task model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth import get_user_model
+from model_utils.models import TimeStampedModel
+
+User = get_user_model()
+
+STATUS_CHOICES = (
+    ("THIS_WEEK", "This Week"),
+    ("DONE", "Done"),
+    ("TODAY", "Today"),
+)
+
+
+class Task(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    task = models.CharField(max_length=140)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    date_done = models.DateTimeField(blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.task
